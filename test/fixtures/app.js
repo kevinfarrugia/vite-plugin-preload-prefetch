3f463cab0bbcfaa3f71589fb7531e0df1c79e523
Kevin Farrugia <hello@imkev.dev>
2025-04-02 12:41:37 +0200

    Updated to support Vite v6; Improved test quality

--- test/fixtures/app.js
+++ test/fixtures/app.js
@@ -4,15 +4,17 @@ import sum from "./sum";
 
 sum(1, 2);
 
-// dynamically import form-validation on interaction with the form
-document.querySelectorAll("#myForm input").addEventListener(
-  "blur",
-  async () => {
-    // Get the form validation named export from the module through destructuring
-    const { validateForm } = await import("./form-validation");
+let formValidationPromise = null;
 
-    // Validate the form
-    validateForm();
-  },
-  { once: true }
+// dynamically import form-validation on interaction with the form
+Array.from(document.querySelectorAll("#myForm input")).forEach((n) =>
+  n.addEventListener("blur", async (e) => {
+    // Import the form-validation module dynamically
+    // This will be a separate chunk in the build
+    // and will only be loaded when the form is interacted with
+    formValidationPromise = import("./form-validation");
+
+    const { default: validateForm } = await formValidationPromise;
+    validateForm(document.querySelector("#myForm"))(e);
+  })
 );
